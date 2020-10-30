858321101eff57caca8757fd198bb6623cab2966
risak10 <koharu10.f@gmail.com>
2020-10-30 18:22:20 +0900

    1st commit

---
+++ FizzBuzz.rb
@@ -0,0 +1,14 @@
+#!/bin/bash/ruby
+
+for num in 1..100 do
+        if num%3 == 0 && num%5 == 0 then
+                puts "FizzBuzz"
+        elsif num%3 == 0 then
+                puts "Fizz"
+        elsif num%5 == 0 then
+                puts "Buzz"
+        else
+                puts "#{num}\n"
+        end
+end
+
